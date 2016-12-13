368c2a5a963c02f0e6d48988969bcdc31649f0af
unknown <eduardo.palermo@m4u.com.br>
2016-12-13 07:43:26 +0100

    Moving on

--- src/main/java/org/circuit/list/PersistedList.java
+++ src/main/java/org/circuit/list/PersistedList.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import org.circuit.util.ByteUtils;
 import org.circuit.util.IoUtils;
 import org.circuit.util.ListUtils;
 
@@ -36,16 +37,13 @@ public class PersistedList<T> implements List<T> {
 		}
 		
 		byte[] data = IoUtils.objectToBytes(e);
+		
+		int newDataLengthSize = ByteUtils.bytesNeededToRepresent(data.length);
+		
+		
 		return true;
 	}
 	
-	private long getPhysicalElementsCount() {
-		try {
-			return randomAccessFile.length() / (dataLengthSize + dataItselfSize);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
 
 	@Override
 	public void add(int index, T element) {
@@ -157,4 +155,77 @@ public class PersistedList<T> implements List<T> {
 		throw new RuntimeException("Not Implemented");
 	}
 
+	
+	
+	private void recreateFile(int newDataLengthSize, int newDataItselfSize) {
+		
+		try {
+			if (needToRecreateFile(newDataLengthSize, newDataItselfSize)) {
+				RandomAccessFile newRandomAccessFile = new RandomAccessFile(ListUtils.generateFile(file), "rw");
+				
+				for (int i = 0; i < getPhysicalElementsCount(); i++) {
+					randomAccessFile.seek(i * getRecordSize());
+					
+					int dataLength = this.getDataLenght(i);
+					
+
+					
+				}
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	private byte[] getBytes(long index) {
+		int dataLenght = getDataLenght(index);
+		return getBytes(index, dataLenght);
+	}
+
+	private byte[] getBytes(long index, int dataLength) {
+		byte array[] = null;
+		try {
+			randomAccessFile.seek(index * getRecordSize() + this.dataLengthSize);
+			array = new byte[dataLength];
+			randomAccessFile.read(array);
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return array;
+	}
+
+	
+	private int getDataLenght(long index) {
+		int answer = 0;
+		try {
+			randomAccessFile.seek(index * getRecordSize());
+			byte array[] = new byte[this.dataLengthSize];
+			randomAccessFile.read(array);
+			answer = ByteUtils.toInt(array);
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return answer;
+	}
+	
+	private boolean needToRecreateFile(int newDataLengthSize, int newDataItselfSize) {
+		return (newDataLengthSize > this.dataLengthSize) || (newDataItselfSize > this.dataItselfSize); 
+	}
+	
+	private long getPhysicalElementsCount() {
+		try {
+			return randomAccessFile.length() / (dataLengthSize + dataItselfSize);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private long getRecordSize() {
+		return this.dataItselfSize + this.dataLengthSize;
+	}
+
+	
 }
--- src/main/java/org/circuit/util/ByteUtils.java
+++ src/main/java/org/circuit/util/ByteUtils.java
@@ -10,7 +10,32 @@ public class ByteUtils {
 			return 1;
 		}
 		return (int) (1 + (Math.log(n)/Math.log(256)));
+	}
+
+	public static int toInt(byte array[]) {
+		int answer = 0;
 		
+		for (int i = 0; i < array.length; i++) {
+			answer += array[i] * Math.pow(256, (array.length - 1) - i);
+		}
+		
+		return answer;
+	}
+
+	public static byte[] toByteArray(int input, int size) {
+		byte[] answer = null;
+		
+		if (size < bytesNeededToRepresent(input)) {
+			throw new RuntimeException("Inconsistency");
+		}
+		
+		answer = new byte[size];
+		
+		for (int i = 0; i < size; i++) {
+			answer[i] = (byte) (Math.pow(256, size - i) % input);
+		}
+		
+		return answer;
 	}
 
 }
